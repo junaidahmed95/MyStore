4ae33cf08e87e94f744a92e14624d71cd10465ac
junaidahmed95 <junaid.ja1995@gmail.com>
2020-04-13 06:27:35 +0500

    updated

--- app/src/main/java/com/example/mystore/Adapter/AllStoreAdapter.java
+++ app/src/main/java/com/example/mystore/Adapter/AllStoreAdapter.java
@@ -1,15 +1,21 @@
 package com.example.mystore.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.cardview.widget.CardView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.example.mystore.AllStoresActivity;
 import com.example.mystore.Model.AllStore;
 import com.example.mystore.R;
 
@@ -41,18 +47,34 @@ public class AllStoreAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
 
         convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.allstoreitem, parent, false);
-
+        LinearLayout mmainLayout = convertView.findViewById(R.id.mainLayout);
+        CardView storecardId = convertView.findViewById(R.id.storecardId);
         ImageView mimg_store = convertView.findViewById(R.id.img_store);
         TextView txt_storedistance = convertView.findViewById(R.id.txt_storedistance);
         TextView txt_storename = convertView.findViewById(R.id.txt_storename);
 
+        storecardId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mcontext, AllStoresActivity.class);
+                intent.putExtra("storeid", storeList.get(position).getStr_id());
+                mcontext.startActivity(intent);
+            }
+        });
+
         Glide.with(mcontext).load(storeList.get(position).getImage()).apply(new RequestOptions().placeholder(R.drawable.avatar)).into(mimg_store);
         txt_storename.setText(storeList.get(position).getName());
+        txt_storedistance.setText(storeList.get(position).getDistance()+"Km");
 
-        //txt_storedistance.setText(storeList.get(position).getDistance());
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mcontext, "" + storeList.get(position).getStr_id(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return convertView;
     }
--- app/src/main/java/com/example/mystore/AllStoresActivity.java
+++ app/src/main/java/com/example/mystore/AllStoresActivity.java
@@ -1,20 +1,56 @@
 package com.example.mystore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.provider.Settings;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 import com.example.mystore.Adapter.AllStoreAdapter;
+import com.example.mystore.Adapter.CategoryAdapter;
 import com.example.mystore.Model.AllStore;
+import com.example.mystore.Model.Category;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -25,14 +61,15 @@ import java.util.List;
 
 public class AllStoresActivity extends AppCompatActivity {
 
-    private String get_store = "https://chhatt.com/Cornstr/grocery/api/get/allstores";
-
+    private final String JSON_URL = "https://chhatt.com/Cornstr/grocery/api/maincat";
     private JsonArrayRequest request;
     private RequestQueue requestQueue;
 
-    private GridView grd_str;
-    private AllStoreAdapter allStoreAdapter;
-    private List<AllStore> storeList;
+
+
+    private List<Category> productList;
+    ProgressBar mprogressbar;
+    private GridView categoryRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,21 +77,21 @@ public class AllStoresActivity extends AppCompatActivity {
         setContentView(R.layout.activity_all_stores);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("Your Store");
+        toolbar.setTitle("Categories");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        productList = new ArrayList<>();
 
-        grd_str = findViewById(R.id.gd1);
-        storeList = new ArrayList<>();
+        categoryRecyclerView = findViewById(R.id.cat_recyclerView);
 
-        getstore();
 
+
+        parseJSON();
     }
 
-    private void getstore() {
-
-        request = new JsonArrayRequest(get_store, new Response.Listener<JSONArray>() {
+    private void parseJSON() {
+        request = new JsonArrayRequest(JSON_URL, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
 
@@ -64,22 +101,19 @@ public class AllStoresActivity extends AppCompatActivity {
                     try {
                         jsonObject = response.getJSONObject(i);
 
-                        String storename = jsonObject.getString("str_name");
-                        String store_id = jsonObject.getString("id");
-                        String distance = jsonObject.getString("address");
-                        String store_image = jsonObject.getString("user_thumb");
-
-
-                        storeList.add(new AllStore(store_image, storename, distance, store_id));
+                        String catTitle = jsonObject.getString("m_name");
+                        String catimage = jsonObject.getString("thumbnail");
+                        productList.add(new Category(catimage, catTitle));
 
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
                 }
-                allStoreAdapter = new AllStoreAdapter(storeList, AllStoresActivity.this);
-                grd_str.setAdapter(allStoreAdapter);
-                allStoreAdapter.notifyDataSetChanged();
 
+
+                CategoryAdapter categoryAdapter = new CategoryAdapter(productList, AllStoresActivity.this);
+                categoryRecyclerView.setAdapter(categoryAdapter);
+                categoryAdapter.notifyDataSetChanged();
 
 
             }
@@ -94,6 +128,18 @@ public class AllStoresActivity extends AppCompatActivity {
         requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(request);
 
+    }
 
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        if (menuItem.getItemId() == android.R.id.home) {
+
+            finish();
+        }
+        return super.onOptionsItemSelected(menuItem);
     }
 }
